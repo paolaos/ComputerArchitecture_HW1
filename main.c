c5322bba8a131970ec235c6a29bbdc0fc84277e8
valeriazamora <valezamora@gmail.com>
2019-08-29 20:41:43 -0600

    Fix stuff and add print

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 /*
+* Arquitectura de Computadores
 * Tarea programada 1
 * Paola Ortega - 
 * Valeria Zamora - B37743
 */
 
 
-#include <mpi.h>
+// #include <mpi.h>
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 
@@ -25,6 +27,7 @@ int greatest_common_factor(int a)
             ans--;
         }
     }
+    // Its a prime number
     if (ans <= 1) {
         ans = a; 
     }
@@ -37,7 +40,6 @@ void generate_forest(int* mat, float f0, float f1, float f2, int size) {
     float random_number = 0.0f;
     for(int i = 0; i < size; ++i) {
         random_number = (float)rand() / RAND_MAX;
-        printf("%f\n",random_number);
         if(random_number < f0) {
             mat[i] = 0;
         } else {
@@ -50,43 +52,60 @@ void generate_forest(int* mat, float f0, float f1, float f2, int size) {
     } 
 }
 
+void print_forest(int* mat, int column_size) {
+    for(int i = 0; i < column_size; ++i) {
+        for(int j = 0; j < column_size; ++j) {
+            printf("%d ", mat[i*column_size+j]);
+        }
+        printf("\n");
+    }
+}
+
 // Funcion para contar la cantidad de 0, 1 y 2 un cuadrante especifico.
+
+
+// Get quadrant number of a given position
+int getQuadrant(int position, int side_size, int quadrant_side_size) {
+    int quadrant_number  = 0;
+
+    return quadrant_number;
+}
 
 int main(int argc,char **argv){
     // Declare variables
 	int x = 0, my_id = 0, p = 0, size = 0;
     int process_size = 0, initial_position = 0, initial_row = 0, initial_column = 0, row_size = 0, number_of_quadrants= 0;
     int quadrant_area = 0;
-    int quadrants_in_a_process = 0;
     int * mat = 0;
     int * process_part = 0;
-    float f0, f1, f2;
+    float f0 = 0.0f, f1 = 0.0f, f2 = 0.0f;
 
-    MPI_Init(&argc,&argv);
+    // MPI_Init(&argc,&argv);
 
     // Get p           
-    MPI_Comm_size(MPI_COMM_WORLD,&p);
+    // MPI_Comm_size(MPI_COMM_WORLD,&p);
 
     // Get x 
+    p = 6; // todo delete this line!
     x = greatest_common_factor(p);
     quadrant_area = p*p;
     row_size = x*p;
     size = row_size * row_size;
-    process_size = size/p;
-    
-    
+    process_size = size / p;
  
     // Get my id
-    MPI_Comm_rank(MPI_COMM_WORLD,&my_id);
+    // MPI_Comm_rank(MPI_COMM_WORLD,&my_id);
 
     // Get initial position of the forest
     initial_position = my_id*process_size;
-    initial_row = (int)(initial_position / row_size;
+
+    initial_row = (int)(initial_position / row_size);
     initial_column = initial_position % row_size;
 
+    process_part = (int *)malloc((process_size)*(sizeof(int)));
 
     // Initialize 
-    if (myid == 0){
+    if (my_id == 0){
        // Create forest
        mat = (int *)malloc((size)*(sizeof(int)));
 
@@ -95,13 +114,13 @@ int main(int argc,char **argv){
        while(!valid_distribution) {
         printf("Enter distribution you want to use to generate the forest. Values must be from 0.0 to 1.0 and must add up 1.0\n");
 
-        printf("Enter the percentage for value 0:");
+        printf("Enter the percentage for value 0: ");
         scanf("%f", &f0);
 
-        printf("Enter the percentage for value 1:)");
+        printf("Enter the percentage for value 1: ");
         scanf("%f", &f1);
 
-        printf("Enter the percentage for value 2:");
+        printf("Enter the percentage for value 2: ");
         scanf("%f", &f2);
 
         float total = f0+f1+f2;
@@ -112,11 +131,12 @@ int main(int argc,char **argv){
 
        // Fill the forest
         generate_forest(mat, f0, f1, f2, size);
+        print_forest(mat, row_size);
     }
 
 
     // Send parts to each process
-    MPI_SCATTER(mat, process_size, MPI_INT, process_part, process_size, MPI_INT, 0, MPI_COMM_WORLD);
+    // MPI_SCATTER(mat, process_size, MPI_INT, process_part, process_size, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Recorrer todo el process_part
 
@@ -128,10 +148,14 @@ int main(int argc,char **argv){
 
 
     // Wait
-	MPI_Barrier(MPI_COMM_WORLD);
+	//MPI_Barrier(MPI_COMM_WORLD);
 
            
-    MPI_Finalize();
+    // MPI_Finalize();
+    if (my_id == 0) {
+        free(mat);
+    }
+    free(process_part);
     return 0;
 }
  
